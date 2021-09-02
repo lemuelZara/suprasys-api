82e152e28f7978727d61f6fa79b5b016b90f40a7
lemuelZara <lemuel.czara@gmail.com>
2021-09-02 14:45:56 -0300

    feat: list products

--- src/main/java/com/example/suprasysapi/controllers/ProductController.java
+++ src/main/java/com/example/suprasysapi/controllers/ProductController.java
@@ -1,14 +1,22 @@
 package com.example.suprasysapi.controllers;
 
+import java.util.Optional;
+
 import com.example.suprasysapi.domain.Product;
 import com.example.suprasysapi.dtos.ProductDTO;
 import com.example.suprasysapi.services.ProductService;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +38,21 @@ public class ProductController {
         service.save(product);
 
         return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<Page<Product>> list(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return new ResponseEntity<>(service.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> show(@PathVariable(value = "id") Integer id) {
+        Optional<Product> productOptional = service.findById(id);
+
+        if (!productOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(productOptional.get());
+        }
     }
 }
--- src/main/java/com/example/suprasysapi/services/ProductService.java
+++ src/main/java/com/example/suprasysapi/services/ProductService.java
@@ -1,9 +1,13 @@
 package com.example.suprasysapi.services;
 
+import java.util.Optional;
+
 import com.example.suprasysapi.domain.Product;
 import com.example.suprasysapi.repositories.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +17,13 @@ public class ProductService {
 
     public Product save(Product product) {
         return repository.save(product);
+    }
+
+    public Optional<Product> findById(Integer id) {
+        return repository.findById(id);
+    }
+
+    public Page<Product> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 }
